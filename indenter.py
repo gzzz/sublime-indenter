7789b891564e743e85666529b422b67d2e5effea
gz <gz@groundzero.ru>
2016-10-26 04:47:09 +0300

    Some fixes.

--- indenter.py
+++ indenter.py
@@ -1,5 +1,6 @@
-import sublime_plugin
+import sublime, sublime_plugin
 import re
+
 
 class Indenter(object):
 	def __init__(self, view):
@@ -17,15 +18,15 @@ class Indenter(object):
 			if self.comment_characters:
 				view.run_command('detect_indentation')
 
-				translate_to_spaces = view.settings().get('translate_tabs_to_spaces', False)
+				use_spaces = view.settings().get('translate_tabs_to_spaces', False)
 
-				if translate_to_spaces:
+				if use_spaces:
 					self.indent_characters = u' ' * view.settings().get('tab_size', 4)
 				else:
 					self.indent_characters = u'	'
 
-				self.commented = re.compile('^(%s ?)(.+)' % self.comment_characters, re.DOTALL)
-				self.indented = re.compile('^(%s)(.+)' % self.indent_characters, re.DOTALL)
+				self.commented = re.compile(u'^(?P<comment>%s+ ?)(?P<tail>.+)' % self.comment_characters, re.DOTALL)
+				self.indented = re.compile(u'^(?P<first_indent>%s)(?P<tail>.+)' % self.indent_characters, re.DOTALL)
 
 #				print 'indenter', self.commented.pattern, self.indented.pattern
 
@@ -45,33 +46,34 @@ class Indenter(object):
 		view = self.view
 
 		selections = view.sel()
-		new_selections = []
+		processed_selections = []
 
 		for region in selections:
 			whole_region = view.full_line(region)
 			text = u''
 			delta = 0
-			offset = len(self.indent_characters)
+			offset = 0
 
 			if not whole_region.empty():
-				lines = view.lines(view.line(region))
+				lines = view.lines(whole_region)
 
 				for line in lines:
-					line_text = view.substr(view.full_line(line))
-					line_text_len = len(line_text)
+					line_str = view.substr(view.full_line(line))
+					line_str_len = len(line_str)
 
-					if len(line_text) > len(self.comment_characters) + 1:
-						line_text = processor(line_text)
-						delta += len(line_text) - line_text_len
+					if len(line_str.replace(self.comment_characters, '').strip()) > 0:
+						line_str = processor(line_str)
+						delta += len(line_str) - line_str_len
 
-					text += line_text
+						if line == lines[0] and delta != 0:
+							offset = len(self.indent_characters)
+
+					text += line_str
 
 				view.replace(edit, whole_region, text)
 
 			if delta < 0:
 				offset = -offset
-			elif delta == 0:
-				offset = 0
 
 			begin = region.begin() + offset
 			end = region.end() + delta
@@ -79,15 +81,16 @@ class Indenter(object):
 			if begin < whole_region.begin():
 				begin = whole_region.begin()
 
-			new_selections.append(sublime.Region(begin, end))
+			processed_selections.append(sublime.Region(begin, end))
 
-		selections.clear()
-		for region in new_selections:
-			selections.add(region)
+		if processed_selections:
+			selections.clear()
+			for region in processed_selections:
+				selections.add(region)
 
 	def indent_line(self, line):
 		if line.startswith(self.comment_characters):
-			line = re.sub(self.commented, u'%s%s%s' % ('\g<1>', self.indent_characters, '\g<2>'), line)
+			line = re.sub(self.commented, u'%s%s%s' % ('\g<comment>', self.indent_characters, '\g<tail>'), line)
 		else:
 			line = u'%s%s' % (self.indent_characters, line)
 
@@ -95,14 +98,14 @@ class Indenter(object):
 
 	def unindent_line(self, line):
 		if line.startswith(self.comment_characters):
-			line_parts = re.findall(self.commented, line)
+			line_parts = re.search(self.commented, line)
 
 			if line_parts:
-				line = line_parts[0][1]
-				line = re.sub(self.indented, u'\g<2>', line)
-				line = u'%s%s' % (line_parts[0][0], line)
+				line = line_parts.group('tail')
+				line = re.sub(self.indented, u'\g<tail>', line)
+				line = u'%s%s' % (line_parts.group('comment'), line)
 		else:
-			line = re.sub(self.indented, u'\g<2>', line)
+			line = re.sub(self.indented, u'\g<tail>', line)
 
 		return line
 
@@ -114,11 +117,11 @@ class IndentCommand(sublime_plugin.TextCommand):
 		self.indenter = Indenter(self.view)
 
 
-class IndentTextCommand(IndentCommand):
+class IndentLinesCommand(IndentCommand):
 	def run(self, edit):
 		self.indenter.indent(edit)
 
 
-class UnindentTextCommand(IndentCommand):
+class UnindentLinesCommand(IndentCommand):
 	def run(self, edit):
 		self.indenter.unindent(edit)
